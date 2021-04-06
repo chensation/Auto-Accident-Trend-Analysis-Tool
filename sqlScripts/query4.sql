26e1538fe7e4489ddecccbbbd31658e9a6bb7dbc
chensation <fogwhite99@gmail.com>
2021-04-06 15:48:16 -0400

    updated query 4, added new query 2 and 3

--- sqlScripts/query4.sql
+++ sqlScripts/query4.sql
@@ -1,36 +1,35 @@
 
 --QUERY 4
-select count(*),TO_CHAR(START_TIME, 'YY-MM') AS MONTH from 
+select count(*)/4,TO_CHAR(START_TIME, 'MM') AS MONTH from 
     SIYUCHEN.ACCIDENT 
 WHERE TO_CHAR(START_TIME, 'YY-MM')<'20-03'
-GROUP BY TO_CHAR(START_TIME, 'YY-MM')
-ORDER BY TO_CHAR(START_TIME, 'YY-MM')
+GROUP BY TO_CHAR(START_TIME, 'MM')
+ORDER BY TO_CHAR(START_TIME, 'MM');
 
-
-select count(*),TO_CHAR(START_TIME, 'YY-MM') AS MONTH from 
+select count(*),TO_CHAR(START_TIME, 'MM') AS MONTH from 
     SIYUCHEN.ACCIDENT 
 WHERE TO_CHAR(START_TIME, 'YY-MM')>='20-03'
-GROUP BY TO_CHAR(START_TIME, 'YY-MM')
-ORDER BY TO_CHAR(START_TIME, 'YY-MM')
+GROUP BY TO_CHAR(START_TIME, 'MM')
+ORDER BY TO_CHAR(START_TIME, 'MM')
 
 
 SELECT AVG((sysdate + (end_time - start_time)*24*60- sysdate)) AS AVG_MINUTE, 
-        TO_CHAR(START_TIME, 'YY-MM') AS H FROM  
+        TO_CHAR(START_TIME, 'MM') AS H FROM  
                 (   
                 SELECT *  FROM SIYUCHEN.ACCIDENT 
                 WHERE TO_CHAR(START_TIME, 'YY-MM')<'20-03' 
                 
                 ) 
-    GROUP BY TO_CHAR(START_TIME, 'YY-MM')
-    ORDER BY TO_CHAR(START_TIME, 'YY-MM')
+    GROUP BY TO_CHAR(START_TIME, 'MM')
+    ORDER BY TO_CHAR(START_TIME, 'MM')
     
     
 SELECT AVG((sysdate + (end_time - start_time)*24*60- sysdate)) AS AVG_MINUTE, 
-        TO_CHAR(START_TIME, 'YY-MM') AS H FROM  
+        TO_CHAR(START_TIME, 'MM') AS H FROM  
                 (   
                 SELECT *  FROM SIYUCHEN.ACCIDENT 
                 WHERE TO_CHAR(START_TIME, 'YY-MM')>='20-03' 
                 
                 ) 
-    GROUP BY TO_CHAR(START_TIME, 'YY-MM')
-    ORDER BY TO_CHAR(START_TIME, 'YY-MM')
+    GROUP BY TO_CHAR(START_TIME, 'MM')
+    ORDER BY TO_CHAR(START_TIME, 'MM')
