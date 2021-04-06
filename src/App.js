1373bbbd38d2d7603fb7ef61fb47a78b602890a1
chensation <fogwhite99@gmail.com>
2021-04-06 19:06:00 -0400

    set climate and hourSeason ready for backend function. changed styling, experimenting with chart size, disabled legend onclick

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import {defaults} from 'react-chartjs-2';
 
 function App() {
+
+  defaults.global.defaultFontSize = 16;
+
   return (
     <div className="App">
       <NavBar></NavBar>
--- src/pages/Climate.js
+++ src/pages/Climate.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -8,44 +8,63 @@ import { Line } from 'react-chartjs-2';
 
 function Climate() {
 
+  const regionLabels = ['Latitudes 25°N - 30°N', 'Latitudes 30°N - 35°N', 'Latitudes 35°N - 40°N', 'Latitudes 40°N - 45°N', 'Latitudes 45°N - 50°N'];
+  const graphLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
   let region1Data = [5,10,15,20,25,30,35,40,9,8,45,100];
   let region2Data = [12, 19, 3, 5, 2, 31, 2, 19, 3, 5, 2, 3];
   let region3Data = [112, 191, 13, 51, 12, 31, 12, 19, 3, 5, 2, 3];
   let region4Data = [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3];
   let region5Data = [1, 90, 39, 58, 28, 38, 128, 189, 38, 58, 28, 38];
 
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      /*place function here
+      region1Data = function(region1) etc
+      or
+      regionsData[i] = function(i) etc
+
+      */
+    }
+
+    fetchData()
+  }, []);
+
+
   let regionsData = [region1Data, region2Data, region3Data, region4Data, region5Data];
 
   let region1 = {
-    label: 'Region 1',
+    label: regionLabels[0],
     data: region1Data,
     fill: false,
     backgroundColor: 'rgb(255, 0, 0)',
     borderColor: 'rgba(255, 0, 0, 0.2)',
   };
   let region2 ={
-    label: 'Region 2',
+    label: regionLabels[1],
     data: region2Data,
     fill: false,
     backgroundColor: 'rgb(255, 127, 0)',
     borderColor: 'rgba(255, 127, 0, 0.2)',
   };
   let region3 = {
-    label: 'Region 3',
+    label: regionLabels[2],
     data: region3Data,
     fill: false,
     backgroundColor: 'rgb(255, 214, 0)',
     borderColor: 'rgba(212, 175, 0, 0.2)',
   };
   let region4 = {
-    label: 'Region 4',
+    label: regionLabels[3],
     data: region4Data,
     fill: false,
     backgroundColor: 'rgb(0, 255, 0)',
     borderColor: 'rgba(0, 255, 0, 0.2)',
   };
   let region5 = {
-    label: 'Region 5',
+    label: regionLabels[4],
     data: region5Data,
     fill: false,
     backgroundColor: 'rgb(0, 0, 255)',
@@ -55,24 +74,39 @@ function Climate() {
   let displayedRegions = [region1,region2,region3,region4,region5];
   
   const [data, setData] = useState({
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    labels: graphLabels,
     datasets: displayedRegions,
   });
 
   const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+
     title: {
       display: true,
-      text: 'Climate Accidents By Region'
+      text: 'Accident Count By Climate Regions'
     },
 
-    scaleLabel:{
-      display:true,
-      labelString: 'Accident Count'
+    legend: {
+        onClick: () => {}
     },
     
     scales: {
+      xAxes: [
+        {
+          scaleLabel:{
+            display:true,
+            labelString: 'Month Of Year'
+          },
+        },
+      ],
       yAxes: [
         {
+          scaleLabel:{
+            display:true,
+            labelString: 'Accident Count'
+          },
+
           ticks: {
             beginAtZero: true,
           },
@@ -82,19 +116,19 @@ function Climate() {
   }
 
   const [checkboxes, setCheckboxes] = useState([
-    { name: 'Region 1',
+    { name: regionLabels[0],
       checked: true
     },
-    { name: 'Region 2',
+    { name: regionLabels[1],
       checked: true
     },
-    { name: 'Region 3',
+    { name: regionLabels[2],
       checked: true
     },
-    { name: 'Region 4',
+    { name: regionLabels[3],
       checked: true
     },
-    { name: 'Region 5',
+    { name: regionLabels[4],
       checked: true
     },
   ]);
@@ -112,13 +146,17 @@ function Climate() {
     updateGraph(temp);
     
     setData({
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      labels: graphLabels,
       datasets: displayedRegions,
     });
     
   }
   
   return (
+    //check if useeffect has passed
+    regionsData.length ?
+    // or regionsData[0].length depending on implementation
+
     <Grid
       container
       direction='row'
@@ -132,18 +170,23 @@ function Climate() {
           justify='center'
           alignItems="center"
         >
-        <FormGroup>
-          {checkboxes.map((checkbox, index)=>
+          <FormGroup>
+            {checkboxes.map((checkbox, index)=>
 
-            <FormControlLabel key={index} control={<Checkbox checked={checkbox.checked} onChange={handleRegionChange} name={index.toString()} color="primary"/>} label={checkbox.name}/>
-          )}
-        </FormGroup>
+              <FormControlLabel key={index} control={<Checkbox checked={checkbox.checked} onChange={handleRegionChange} name={index.toString()} color="primary"/>} label={checkbox.name}/>
+            )}
+          </FormGroup>
         </Grid>
       </Grid>
-      <Container fixed >
-        <Line data={data} options={options}/>
-      </Container>
+      <Grid item>
+        <Container>
+          <Line data={data} options={options} width={1200} height={700}/>
+        </Container>
+      </Grid>
+      
     </Grid>
+
+    : <div></div>
   );
 }
 
