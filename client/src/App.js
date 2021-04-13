ade133e593014252a96fa53fb211c12d84e85793
chensation <fogwhite99@gmail.com>
2021-04-13 15:53:33 -0400

    backend server working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import {defaults} from 'react-chartjs-2';
@@ -7,8 +7,21 @@ function App() {
 
   defaults.global.defaultFontSize = 16;
 
+  const [apiResponse, setApiResponse] = useState("");
+
+  const callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => setApiResponse(res));
+  }
+
+  
+  useEffect(()=>{
+    callAPI()
+  });
   return (
     <div className="App">
+      <p className="App-intro">{apiResponse}</p>
       <NavBar></NavBar>
     </div>
   );
